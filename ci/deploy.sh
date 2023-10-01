8d3134398de282a8f9d0ed4e6855ad2a43664086
Aastha <aastha.jainie@gmail.com>
2023-09-30 21:09:42 -0500

    Changed folder name to StealthSite for sync

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
 
 echo "starting code deployment in process ID: $$"
-cd ~/vicinity
+cd ~/StealthSite
 git pull
-gsutil rsync -rd -x "\.git|.*\.log$" . gs://code.pakasa.world/vicinity
-cd ~/vicinity/web
-gsutil rsync -rd -x "\.git|.*\.sh$|.*\.log$" . gs://cnt.pakasa.world
+gsutil rsync -rd -x "\.git|.*\.sh$|.*\.log$" . gs://stealth-robotics
